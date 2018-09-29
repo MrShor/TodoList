513a926f7587f1de6df3d58f19fd737f9166f7c5
F.B.I <shorshalka.home@gmail.com>
2018-09-30 00:12:12 +0300

    My first commit

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app" class="container">
+    <img src="./assets/logo.png" class="logo">
+    <!-- this components off to choose from -->
+    <!-- <HelloWorld/> -->
+    <!-- <hello-world></hello-world> -->
+    <TodoList/>
+    <!-- <todo-list/> -->
+    <!-- Equivalent, Why??? -->
+    <!-- <hello-world></hello-world> </hello-world> <HelloWorld/>-->
+    <!-- <todo-list></todo-list> === <todo-list/> === <TodoList/> -->
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorldName'
+import TodoList from './components/TodoList'
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    TodoList,
+  }
+}
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+.container {
+  max-width: 600px;
+  margin: 0 auto;
+}
+.logo {
+  display: block;
+  margin: 20px auto;
+  height: 100px;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /* text-align: center; */
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <legend class="header">Todo here!</legend>
+    <input type="text" class="todo-input" placeholder="What need to be done" v-model="newTodo" @keyup.enter="addTodo"> 
+    <!-- @keyup.enter="addTodo" ==> methodos: {addTodo(){alert('adding')}} -->
+    <div v-for="todo in todos" :key="todo.id" class="todo-item">
+      {{ todo.title }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'todo-list', //What this???
+  data () {
+    return {
+      newTodo: '',
+      idForTodo: 3,
+      todos: [
+        {
+          'id': 1,
+          title: 'Finish Vue Screencst',
+          'completed':false,
+        },
+        {
+          'id': 2,
+          title: 'Take over world',
+          'completed':false,
+        },
+      ]
+    }
+  },
+  methods: {
+    addTodo() {
+      // alert('adding')  //Real Working!
+      this.todos.push({
+        id: this.idForTodo,
+        title: this.newTodo,
+        completed: false,
+      })
+      this.newTodo = ''
+      this.idForTodo++
+    }
+  }
+}
+</script>
+
+
+<style lang="scss">
+.header {
+  text-align: center;
+  margin: 16px;
+  // paddin g: 16px;
+}
+  .todo-input {
+    width: 100%;
+    padding: 10px 18px;
+    font-size: 18px;
+    margin-bottom: 16px;
+    &:focus {
+      outline: 0;
+    }
+  }
+</style>
